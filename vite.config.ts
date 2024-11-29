c9129ae062dcff23d46b151b96f2de8aa08e366e
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2024-11-29 21:44:26 +0300

    Adds Rollup visualizer and optimizes module bundling
    Improves build performance by optimizing module bundling and adding a Rollup visualizer plugin.
    Adds explicit external dependencies for Stripe and other libraries.
    Configures module chunking strategy for improved loading performance.

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,46 @@
+/** @format */
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
 	plugins: [react()],
 	optimizeDeps: {
+		include: ["@reduxjs/toolkit"],
 		exclude: ["lucide-react"],
-		include: ["@stripe/react-stripe-js", "@stripe/stripe-js"],
 	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
+			path: "path-browserify",
 		},
 	},
 	build: {
-		chunkSizeWarningLimit: 1200,
 		rollupOptions: {
 			external: ["@stripe/react-stripe-js", "@stripe/stripe-js"],
 			output: {
 				manualChunks: {
 					react: ["react", "react-dom"],
-					vendor: ["lodash", "axios"],
-					stripe: ["@stripe/react-stripe-js", "@stripe/stripe-js"],
+					redux: ["@reduxjs/toolkit", "react-redux", "redux-persist"],
+					axios: ["axios"],
+					clsx: ["clsx"],
+					datefns: ["date-fns"],
+					framer: ["framer-motion"],
+					inputotp: ["input-otp"],
+					lucide: ["lucide-react"],
+					radix: [
+						"@radix-ui/react-dialog",
+						"@radix-ui/react-label",
+						"@radix-ui/react-select",
+						"@radix-ui/react-slot",
+					],
+					swiper: ["swiper"],
+					recharts: ["recharts"],
+					zod: ["zod"],
+					tailwindmerge: ["tailwind-merge"],
+					zustand: ["zustand"],
 				},
 			},
 		},
